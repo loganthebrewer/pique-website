c130c82594c196f9bde399b1c0d540d72c35c92e
loganthebrewer <loganthebrewer@gmail.com>
2025-08-30 16:37:05 -0500

    Initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import UnderwaterText from "@/components/UnderwaterText";
+
+export default function Hero() {
+    return (
+        <section className="bg-[url('https://i.scdn.co/image/ab6761670000ecd4717ab4bb42b7a6a3617c0240')] bg-cover bg-center min-h-screen flex justify-center items-center">
+            <UnderwaterText>
+                PIQUE
+            </UnderwaterText>
+        </section>
+    );
+}
