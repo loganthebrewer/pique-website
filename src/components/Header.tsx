e3824caf2e9e2e4f07e2ad58a5467f861784c45b
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:03:12 -0500

    Added PiqueTypeface component to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import SocialButtonRow from "@/components/SocialButtonRow";
 import Link from "next/link";
+import PiqueTypeface from "@/components/PiqueTypeface";
 
 export default function Header() {
     return (
         <header className="bg-black sticky top-0 z-50">
             <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
-                <a href="#" className="font-display text-3xl">PIQUE</a>
-                <div className="hidden md:flex items-center space-x-8">
+                <a href="#" className="font-display text-3xl">
+                    <PiqueTypeface/>
+                </a>
+                <div className="hidden md:flex items-center space-x-8 font-semibold">
                     <Link href="/#events" className="text-neutral-300 nav-link">Shows</Link>
                     <Link href="/#music" className="text-neutral-300 nav-link">Music</Link>
                     <Link href="/#merch" className="text-neutral-300 nav-link">Merch</Link>
