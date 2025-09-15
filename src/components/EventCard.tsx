35bcc7ebcfa1e392a411af4afed6f67bd30a3422
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:05:44 -0500

    Recolored event card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -2,8 +2,8 @@ import Typography from "@/components/Typography";
 
 export default function EventCard() {
     return(
-        <div className="border-amber-300 border w-full bg-neutral-900 rounded-3xl p-5">
-            <Typography variant={"small"} className="text-blue-300">October 9th, 2025</Typography>
+        <div className="border-red-400 border w-full bg-neutral-900 rounded-3xl p-5">
+            <Typography variant={"small"} className="text-red-400">October 9th, 2025</Typography>
             <Typography variant={"h3"}>Rubber Gloves | Denton, TX</Typography>
         </div>
     )
