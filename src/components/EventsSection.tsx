5f8629bd26b0cec9c25e7bc9e00d52d7cf1abc8d
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:10:51 -0500

    Events: Removed visible TODO, added vertical margin

--- src/components/EventsSection.tsx
+++ src/components/EventsSection.tsx
@@ -6,10 +6,7 @@ export default function EventsSection() {
       <section id="events" className="bg-neutral-1000 p-7 md:p-20">
           <Typography variant="h1">Events</Typography>
           <Typography variant="h2">Pique your Poison...</Typography>
-          <div className="flex flex-col items-center">
-              <Typography variant="body">
-                  TODO: Integrate Bandsintown widget here!
-              </Typography>
+          <div className="flex flex-col items-center my-3">
               <EventCard/>
           </div>
       </section>
