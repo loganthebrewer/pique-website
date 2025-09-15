37e5dd2b2e1127eaa8744064ebbcdac371a122cd
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:03:42 -0500

    Added padding and background to merch section

--- src/components/MerchSection.tsx
+++ src/components/MerchSection.tsx
@@ -2,7 +2,7 @@ import Typography from "@/components/Typography";
 
 export default function MerchSection() {
     return(
-        <section>
+        <section className="bg-neutral-1000 p-7 md:p-20">
             <Typography variant="h1">Merch</Typography>
         </section>
     )
