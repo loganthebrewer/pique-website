ed0027e947d87c231fa69af7731e58694ac9084d
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:04:44 -0500

    Changed typeface font thickness

--- src/components/PiqueTypeface.tsx
+++ src/components/PiqueTypeface.tsx
@@ -1,6 +1,6 @@
 export default function PiqueTypeface() {
     return(
-        <h1 className="font-display text-3xl">
+        <h1 className="font-display font-medium text-4xl">
             PIQUE
         </h1>
     )
