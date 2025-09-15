6c61f20f1474725b283c44a643b252d16a62e1fd
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:10:27 -0500

    Added vertical margin to event card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -2,7 +2,7 @@ import Typography from "@/components/Typography";
 
 export default function EventCard() {
     return(
-        <div className="border-red-400 border w-full bg-neutral-900 rounded-3xl p-5">
+        <div className="border-red-400 border w-full bg-neutral-900 rounded-3xl p-5 my-2">
             <Typography variant={"small"} className="text-red-400">October 9th, 2025</Typography>
             <Typography variant={"h3"}>Rubber Gloves | Denton, TX</Typography>
         </div>
