c213abc275cfcc69002fca4962f5b12762541e72
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-09-15 12:18:15 -0500

    Fixed image title capitalization

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import UnderwaterText from "@/components/UnderwaterText";
 
 export default function Hero() {
     return (
-        <section className="bg-[url('/images/band/neon-glass.jpg')] md:bg-[url('/images/band/neon-glass-horizontal.jpg')] bg-cover bg-center min-h-screen flex justify-center items-center">
+        <section className="bg-[url('/images/Band/neon-glass.jpg')] md:bg-[url('/images/Band/neon-glass-horizontal.jpg')] bg-cover bg-center min-h-screen flex justify-center items-center">
             <UnderwaterText>
                 PIQUE
             </UnderwaterText>
